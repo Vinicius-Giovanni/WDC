3fb119b0ed85bccaac791151b62a205e34766a2d
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-01-28 12:28:55 -0300

    Configuração de Browser - Configuração de instância

--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from config.pipeline_config import LINKS
 from pathlib import Path
 import tempfile
 import uuid
@@ -33,6 +34,64 @@ def init_browser(download_dir: str | Path):
             '--disable-extensions',
             '--no-sandbox',
             '--disable-gpu',
-            '--disable-dev-shm-usage'
+            '--disable-dev-shm-usage',
+            '--disable-blink-features=AutomationControlled'
         ]
     )
+
+    return playwright, context
+
+def create_authenticated_page(cookies: list[dict], download_dir: Path):
+    """
+    Cria uma página playwright já autenticada via cookies
+
+    cookies: list[dict]
+    Recebe os cookies em formato de dicionário
+
+    download_dir: Path
+    Recebe o path do diretório de download que será configurado na instância
+    """
+
+    playwright, context = init_browser(download_dir=download_dir)
+
+    page = context.new_page()
+    page.goto(LINKS['LOGIN_CSI'])
+
+    formatted_cookies = []
+
+    for cookie in cookies:
+        try:
+            cookie_copy = cookie.copy()
+
+            # Selenium -> Playwright
+            if 'expiry' in cookie_copy:
+                cookie_copy['expires'] = cookie_copy.pop('expiry')
+
+            # Remove sameSite se existir (segurança)
+            cookie_copy.pop('sameSite', None)
+
+            # Playwright exige domain ou url
+            if 'domain' not in cookie_copy:
+                cookie_copy['url'] = LINKS['LOGIN_CSI']
+
+            formatted_cookies.append(cookie_copy)
+
+        except Exception as e:
+            logger.warning(
+                f'cookie inválido descartado {cookie} motivo {e}',
+                extra={
+                    'job': 'create_authenticated_driver',
+                    'status': 'failure'
+                })
+            
+    context.add_cookies(formatted_cookies)
+
+    # Reload da página já autenticada
+    page.goto(LINKS['LOGIN_CSI'])
+
+    return page, context, playwright
+
+def load_cookies(path='cookies.json'):
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+    
